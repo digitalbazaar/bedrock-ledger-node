9779d9b4fb7db4cbe6e1e8a7b0418cd6dbb367d4
Manu Sporny <msporny@digitalbazaar.com>
2017-05-11 13:41:22 -0400

    Add authorization test mocks.

---
+++ test/mocha/70-authorization.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
+ */
+/* globals should */
+'use strict';
+
+const async = require('async');
+const bedrock = require('bedrock');
+const brIdentity = require('bedrock-identity');
+const brLedger = require('bedrock-ledger');
+const expect = global.chai.expect;
+const helpers = require('./helpers');
+const jsigs = require('jsonld-signatures');
+const mockData = require('./mock.data');
+const uuid = require('uuid/v4');
+
+const baseUri = 'http://example.com';
+
+// use local JSON-LD processor for signatures
+jsigs.use('jsonld', bedrock.jsonld);
+
+describe('authorizeBlock API', () => {
+  before(done => {
+    helpers.prepareDatabase(mockData, done);
+  });
+  beforeEach(done => {
+    helpers.removeCollection('ledger_testLedger', done);
+  });
+  describe('multisignature', () => {
+    it.skip('allows a multisignature that is exactly the threshold', done => {
+      done();
+    });
+    it.skip('allows a multisignature above the threshold', done => {
+      done();
+    });
+    it.skip('denies a multisignature below the threshold', done => {
+      done();
+    });
+  });
+  describe('argon2d proof of work', () => {
+    it.skip('allows a valid proof of work', done => {
+      done();
+    });
+    it.skip('denies an invalid proof of work', done => {
+      done();
+    });
+  });
+});
