d54bbedb9484fe7acb7cdc9eebf8f4bce81d3f08
Ganesh Annan <gannan@digitalbazaar.com>
2021-01-29 18:18:04 -0500

    Add operation.exists API.

--- test/mocha/35-operations-api.js
+++ test/mocha/35-operations-api.js
@@ -32,123 +32,163 @@ describe('Operations API', () => {
       ledgerNode = await brLedgerNode.add(
         actor, {ledgerConfiguration: signedConfig});
     });
-    it('should add operation with optional creator', async function() {
-      const testOperation = {
-        '@context': 'https://w3id.org/webledger/v1',
-        type: 'CreateWebLedgerRecord',
-        creator: 'https://example.com/someCreatorId',
-        record: {
-          '@context': 'https://schema.org/',
-          id: 'urn:uuid:' + uuid(),
-          value: uuid()
-        }
-      };
-      const operation = await helpers.signDocument({
-        doc: testOperation,
-        privateKeyPem: mockData.groups.authorized.privateKey,
-        creator: 'did:v1:53ebca61-5687-4558-b90a-03167e4c2838/keys/144'
-      });
-      await ledgerNode.operations.add({operation});
-    });
-    it('should add operation without optional creator', async function() {
-      const testOperation = {
-        '@context': 'https://w3id.org/webledger/v1',
-        type: 'CreateWebLedgerRecord',
-        // the optional creator is missing
-        record: {
-          '@context': 'https://schema.org/',
-          id: 'urn:uuid:' + uuid(),
-          value: uuid()
-        }
-      };
-      const operation = await helpers.signDocument({
-        doc: testOperation,
-        privateKeyPem: mockData.groups.authorized.privateKey,
-        creator: 'did:v1:53ebca61-5687-4558-b90a-03167e4c2838/keys/144'
-      });
-      await ledgerNode.operations.add({operation});
-    });
-    it('should fail add operation with an incorrect context', async () => {
-      const testOperation = {
-        '@context': 'https://w3id.org/test/v1',
-        type: 'CreateWebLedgerRecord',
-        creator: 'https://example.com/someCreatorId',
-        record: {
-          '@context': 'https://schema.org/',
-          id: 'urn:uuid:' + uuid(),
-          value: uuid()
-        }
-      };
-      const operation = await helpers.signDocument({
-        doc: testOperation,
-        privateKeyPem: mockData.groups.authorized.privateKey,
-        creator: 'did:v1:53ebca61-5687-4558-b90a-03167e4c2838/keys/144'
-      });
-      let err;
-      try {
+    describe('add API', () => {
+
+      it('should add operation with optional creator', async function() {
+        const testOperation = {
+          '@context': 'https://w3id.org/webledger/v1',
+          type: 'CreateWebLedgerRecord',
+          creator: 'https://example.com/someCreatorId',
+          record: {
+            '@context': 'https://schema.org/',
+            id: 'urn:uuid:' + uuid(),
+            value: uuid()
+          }
+        };
+        const operation = await helpers.signDocument({
+          doc: testOperation,
+          privateKeyPem: mockData.groups.authorized.privateKey,
+          creator: 'did:v1:53ebca61-5687-4558-b90a-03167e4c2838/keys/144'
+        });
         await ledgerNode.operations.add({operation});
-      } catch(e) {
-        err = e;
-      }
-      err.name.should.equal('SyntaxError');
-      err.message.should.equal(
-        'Operation context must be "https://w3id.org/webledger/v1"');
-    });
-    it('should fail to add operation w/ incorrect context order', async () => {
-      const testOperation = {
-        '@context': ['https://w3id.org/test/v1'],
-        type: 'CreateWebLedgerRecord',
-        creator: 'https://example.com/someCreatorId',
-        record: {
-          '@context': 'https://schema.org/',
-          id: 'urn:uuid:' + uuid(),
-          value: uuid()
-        }
-      };
-      const operation = await helpers.signDocument({
-        doc: testOperation,
-        privateKeyPem: mockData.groups.authorized.privateKey,
-        creator: 'did:v1:53ebca61-5687-4558-b90a-03167e4c2838/keys/144'
       });
-      let err;
-      try {
+      it('should add operation without optional creator', async function() {
+        const testOperation = {
+          '@context': 'https://w3id.org/webledger/v1',
+          type: 'CreateWebLedgerRecord',
+          // the optional creator is missing
+          record: {
+            '@context': 'https://schema.org/',
+            id: 'urn:uuid:' + uuid(),
+            value: uuid()
+          }
+        };
+        const operation = await helpers.signDocument({
+          doc: testOperation,
+          privateKeyPem: mockData.groups.authorized.privateKey,
+          creator: 'did:v1:53ebca61-5687-4558-b90a-03167e4c2838/keys/144'
+        });
         await ledgerNode.operations.add({operation});
-      } catch(e) {
-        err = e;
-      }
-      err.name.should.equal('SyntaxError');
-      err.message.should.equal('Operation context must contain ' +
-        '"https://w3id.org/webledger/v1" as the first element.');
-    });
-    it('should get event containing the operation', async function() {
-      const testOperation = {
-        '@context': 'https://w3id.org/webledger/v1',
-        type: 'CreateWebLedgerRecord',
-        creator: 'https://example.com/someCreatorId',
-        record: {
-          '@context': 'https://schema.org/',
-          id: 'urn:uuid:' + uuid(),
-          value: uuid()
-        }
-      };
-      const operation = await helpers.signDocument({
-        doc: testOperation,
-        privateKeyPem: mockData.groups.authorized.privateKey,
-        creator: 'did:v1:53ebca61-5687-4558-b90a-03167e4c2838/keys/144'
       });
-      await ledgerNode.operations.add({operation});
-      // unilateral consensus allows immediate retrieval of an event with
-      // a single operation in it from the latest block
-      const result = await ledgerNode.blocks.getLatest();
-      should.exist(result);
-      should.exist(result.eventBlock);
-      should.exist(result.eventBlock.block);
-      should.exist(result.eventBlock.block.event);
-      const event = result.eventBlock.block.event[0];
-      should.exist(event);
-      should.exist(event.operation);
-      should.exist(event.operation[0]);
-      event.operation[0].should.deep.equal(operation);
+      it('should fail add operation with an incorrect context', async () => {
+        const testOperation = {
+          '@context': 'https://w3id.org/test/v1',
+          type: 'CreateWebLedgerRecord',
+          creator: 'https://example.com/someCreatorId',
+          record: {
+            '@context': 'https://schema.org/',
+            id: 'urn:uuid:' + uuid(),
+            value: uuid()
+          }
+        };
+        const operation = await helpers.signDocument({
+          doc: testOperation,
+          privateKeyPem: mockData.groups.authorized.privateKey,
+          creator: 'did:v1:53ebca61-5687-4558-b90a-03167e4c2838/keys/144'
+        });
+        let err;
+        try {
+          await ledgerNode.operations.add({operation});
+        } catch(e) {
+          err = e;
+        }
+        err.name.should.equal('SyntaxError');
+        err.message.should.equal(
+          'Operation context must be "https://w3id.org/webledger/v1"');
+      });
+      it('should fail to add operation w/ incorrect context order',
+        async () => {
+          const testOperation = {
+            '@context': ['https://w3id.org/test/v1'],
+            type: 'CreateWebLedgerRecord',
+            creator: 'https://example.com/someCreatorId',
+            record: {
+              '@context': 'https://schema.org/',
+              id: 'urn:uuid:' + uuid(),
+              value: uuid()
+            }
+          };
+          const operation = await helpers.signDocument({
+            doc: testOperation,
+            privateKeyPem: mockData.groups.authorized.privateKey,
+            creator: 'did:v1:53ebca61-5687-4558-b90a-03167e4c2838/keys/144'
+          });
+          let err;
+          try {
+            await ledgerNode.operations.add({operation});
+          } catch(e) {
+            err = e;
+          }
+          err.name.should.equal('SyntaxError');
+          err.message.should.equal('Operation context must contain ' +
+          '"https://w3id.org/webledger/v1" as the first element.');
+        });
+      it('should get event containing the operation', async function() {
+        const testOperation = {
+          '@context': 'https://w3id.org/webledger/v1',
+          type: 'CreateWebLedgerRecord',
+          creator: 'https://example.com/someCreatorId',
+          record: {
+            '@context': 'https://schema.org/',
+            id: 'urn:uuid:' + uuid(),
+            value: uuid()
+          }
+        };
+        const operation = await helpers.signDocument({
+          doc: testOperation,
+          privateKeyPem: mockData.groups.authorized.privateKey,
+          creator: 'did:v1:53ebca61-5687-4558-b90a-03167e4c2838/keys/144'
+        });
+        await ledgerNode.operations.add({operation});
+        // unilateral consensus allows immediate retrieval of an event with
+        // a single operation in it from the latest block
+        const result = await ledgerNode.blocks.getLatest();
+        should.exist(result);
+        should.exist(result.eventBlock);
+        should.exist(result.eventBlock.block);
+        should.exist(result.eventBlock.block.event);
+        const event = result.eventBlock.block.event[0];
+        should.exist(event);
+        should.exist(event.operation);
+        should.exist(event.operation[0]);
+        event.operation[0].should.deep.equal(operation);
+      });
+    });
+
+    describe('exists API', () => {
+      let recordId;
+
+      before(async () => {
+        recordId = 'urn:uuid:' + uuid();
+
+        const testOperation = {
+          '@context': 'https://w3id.org/webledger/v1',
+          type: 'CreateWebLedgerRecord',
+          creator: 'https://example.com/someCreatorId',
+          record: {
+            '@context': 'https://schema.org/',
+            id: recordId,
+            value: uuid()
+          }
+        };
+        const operation = await helpers.signDocument({
+          doc: testOperation,
+          privateKeyPem: mockData.groups.authorized.privateKey,
+          creator: 'did:v1:53ebca61-5687-4558-b90a-03167e4c2838/keys/144'
+        });
+
+        await ledgerNode.operations.add({operation});
+      });
+
+      it('should find record that exists', async () => {
+        const exists = await ledgerNode.operations.exists({recordId});
+        exists.should.equal(true);
+      });
+
+      it('should not find record that doesn\'t exist', async () => {
+        const exists = await ledgerNode.operations.exists({recordId: 'foobar'});
+        exists.should.equal(false);
+      });
     });
   });
 });
